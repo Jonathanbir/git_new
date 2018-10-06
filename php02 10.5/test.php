865412dc52ee305799469edb5eb6254798271d59
Jonathanbir <jonathan79111@gmail.com>
2018-10-06 16:41:30 +0800

    Add files via upload

---
+++ php02 10.5/test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Examples</title>
+<!-- 寫於2018秋 -->
+<style type="text/css">
+/*#timePanel {
+	color:blue;  
+}	*/
+</style>
+</head>
+<body>
+
+<?php 
+
+?>
+
+
+
+</body>
+</html>
