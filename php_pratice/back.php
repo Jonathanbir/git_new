d08210385414ed3a682debcc6203943c9efc56e7
Jonathanbir <jonathan79111@gmail.com>
2018-10-06 16:41:59 +0800

    Add files via upload

---
+++ php_pratice/back.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<?php
+
+//------------wrap1----------------//
+
+// header('Content-Type:text/html;charser=UTF-8');
+// $account=$_REQUEST["memId"];
+// $password=$_REQUEST["memPsw"];
+// $email=$_REQUEST["mail"];
+// $member_serial=$_REQUEST["member_serial"];
+
+// echo"<br>";
+// echo "暱稱:" .$member_serial."<br>";
+// echo "帳號:" .$memId."<br>";
+// echo "密碼:" .$memPsw."<br>";
+// echo "信箱:" .$email."<br>";
+
+
+// echo "帳號: ",$_REQUEST["memId"],"<br>";
+// echo "密碼: ",$_REQUEST["memPsw"],"<br>";
+// echo "信箱: ",$_REQUEST["mail"],"<br>";
+// echo "暱稱: ",$_REQUEST["member_serial"],"<br>";
+// phpinfo();
+
+//------------wrap2----------------//
+
+// echo "帳號: ",$_REQUEST["memId"],"<br>";
+// echo "密碼: ",$_REQUEST["memPsw"],"<br>";
+
+// echo "說什麼111 : <br>",$_REQUEST["note"],"<hr>";
+// echo "說什麼222:<br>轉過後<br>", nl2br($_REQUEST["note"]),"<hr>";
+
+//------------wrap3----------------//
+
+
+?>
+
+</body>
+</html>
